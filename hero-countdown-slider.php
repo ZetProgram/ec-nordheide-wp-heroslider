51f14ceea12d6ccae4bc08e32b8bfdf04e55d809
Zet <xZetGamerHDx@gmail.com>
2025-08-31 20:37:35 +0200

    Update Version to 1.1.6

--- hero-countdown-slider.php
+++ hero-countdown-slider.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Hero Countdown Slider
  * Description: Hero-Bannerslider mit Countdown/CTA. Slides im Backend pflegen, Block zeigt eine gewählte Slider-Gruppe.
- * Version: 1.1.5
+ * Version: 1.1.6
  * Author: Fabian Bross
  * Requires at least: 5.8
  * Tested up to: 6.6
